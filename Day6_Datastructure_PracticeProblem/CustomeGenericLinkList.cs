2d78b48404f84b5b3f891d85f87bcf26e89b92b4
mahesh <gangurdemahesh9@gmail.com>
2023-03-27 19:18:06 +0530

    [mahesh]add program file for Custom Generic LinkList

---
+++ Day6_Datastructure_PracticeProblem/CustomeGenericLinkList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Datastructure_PracticeProblem
+{
+    class CustomeGenericLinkList<T>
+    {
+        private Node<T> head;
+
+        public void AddLast(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            if (head == null)
+            {
+                head = newNode;
+                Console.WriteLine("{0} is inserted into Linklist", head.data);
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+                Console.WriteLine("{0} is inserted into Linklist", newNode.data);
+            }
+        }
+
+        public void Addfirst(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            Console.WriteLine("{0} is inserted in linklist", newNode.data);
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public void DisplayList()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                Console.Write(current.data + " ");
+                current = current.next;
+            }
+        }
+
+        public void Remove(T data)
+        {
+            if (head == null)
+            {
+                return;
+            }
+
+            if (head.data.Equals(data))
+            {
+                head = head.next;
+                return;
+            }
+
+            Node<T> currentNode = head;
+            while (currentNode.next != null)
+            {
+                if (currentNode.next.data.Equals(data))
+                {
+                    currentNode.next = currentNode.next.next;
+                    return;
+                }
+                currentNode = currentNode.next;
+            }
+        }
+    }
+}
--- Day6_Datastructure_PracticeProblem/Program.cs
+++ Day6_Datastructure_PracticeProblem/Program.cs
@@ -11,6 +11,30 @@ namespace Day6_Datastructure_PracticeProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Day6 Datastructure PracticeProblem");
+            Console.WriteLine("Select any one option for \n 1. Generic Custom Linklist");
+            int operation = Convert.ToInt32(Console.ReadLine());
+            switch (operation)
+            {
+                case 1:
+                    CustomeGenericLinkList<int> integerLinkList = new CustomeGenericLinkList<int>();
+                    integerLinkList.AddLast(10);
+                    integerLinkList.AddLast(20);
+                    integerLinkList.Addfirst(30);
+                    integerLinkList.Addfirst(40);
+                    integerLinkList.DisplayList();
+
+                    CustomeGenericLinkList<string> stringLinkList = new CustomeGenericLinkList<string>();
+                    stringLinkList.AddLast("mahesh");
+                    stringLinkList.Addfirst("Gangurde");
+                    stringLinkList.DisplayList();
+                    Console.WriteLine("after removing Element");
+                    stringLinkList.Remove("Gangurde");
+                    stringLinkList.DisplayList();
+                    break;
+                default:
+                    Console.WriteLine("Please Enter Valid Option");
+                    break;
+            }
         }
     }
 }

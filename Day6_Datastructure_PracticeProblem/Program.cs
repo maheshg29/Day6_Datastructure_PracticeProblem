9af3a970391a78a715ffc17f18d32e6ad516a312
mahesh <gangurdemahesh9@gmail.com>
2023-03-27 19:11:34 +0530

    [mahesh]add Welcome Greeting Message

---
+++ Day6_Datastructure_PracticeProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Datastructure_PracticeProblem
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day6 Datastructure PracticeProblem");
+        }
+    }
+}
